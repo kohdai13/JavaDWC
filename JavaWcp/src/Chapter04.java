be9cfbdc9b0e72c912fa2822118c2d819ac84395
内山昂大 <kohdai@uchiyamakoudainoMacBook-Pro.local>
2022-04-21 18:15:24 +0900

    DMM

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,14 @@
+
+public class Chapter04 {
+	public static void main(String[]args) {
+		String name = "内山";
+		System.out.println(name);
+		
+		name = "Happy";
+		System.out.println(name);
+		
+		int luckyNumber = 7;
+		System.out.println(luckyNumber);
+	}
+
+}
