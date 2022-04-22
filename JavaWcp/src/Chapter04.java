341c18b826bf4d96109369533802ae5e34dea252
内山昂大 <kohdai@uchiyamakoudainoMacBook-Pro.local>
2022-04-22 13:27:20 +0900

    5-8 finish

--- JavaWcp/src/Chapter04.java
+++ JavaWcp/src/Chapter04.java
@@ -1,14 +1,15 @@
 
 public class Chapter04 {
 	public static void main(String[]args) {
+		final int LUCKY_NUMBER = 7;
+		
 		String name = "内山";
 		System.out.println(name);
 		
 		name = "Happy";
 		System.out.println(name);
 		
-		int luckyNumber = 7;
-		System.out.println(luckyNumber);
+		System.out.println(LUCKY_NUMBER);
 	}
 
 }
---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,29 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 123456789123456789L;
+		System.out.println(long1);
+		
+		double double1 = 1.23;
+		System.out.println(double1);
+		
+		boolean b = true;
+		System.out.println(b);
+		
+		Integer i = 100;
+        System.out.println(i);
+        
+        Float f = 1.4142f;
+        System.out.println(f);
+        
+        Character c = 'う';
+        System.out.println(c);
+        
+        int[] array = {0, 10,20,30,40,50};
+        System.out.println(array[3]);
+		
+		
+		
+	}
+
+}
